e37aa308e99c401e8b97a6d59b51695290bc382c
Burgyn <mino.martiniak@gmail.com>
2020-05-25 21:10:25 +0200

    PetsStore

---
+++ src/Services/Sample.PetStore/Startup.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
+
+namespace PetstoreService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("swagger.json", "My API V1");
+            });
+
+            app.UseStaticFiles();
+        }
+    }
+}
